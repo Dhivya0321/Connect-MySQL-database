653674b57f5aab7846dad9a0586b215d75a18e89
Dhivyavarshini <732125104021.25.cse@nandhatech.org>
2026-06-20 11:40:31 +0530

    Add files via upload

---
+++ python40.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'studentdb',
+        'USER': 'root',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '3306',
+    }
+}
